dacf09266c7120c749e18f8c770e77694aa894f9
portgazdbeel <portgaz77@gmail.com>
2021-08-26 16:53:48 +0700

    commit

---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $pesanan;
+    public $status;
+    public $title;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($title,$pesanan, $status)
+    {
+        //
+        $this->pesanan = $pesanan;
+        $this->status = $status;
+        $this->title = $title;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $details = [
+            'title' => $this->title,
+            'data'  => $this->pesanan,
+            'status' => $this->pesanan
+        ];
+
+        return $this->view('email.email')->with($details);
+    }
+}
